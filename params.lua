cc2c3995d67d6d757732f63e2f4eb824ab9629d1
Michiel Fokke <github@fokke.org>
2016-03-19 15:27:18 +0100

    added parameter file

---
+++ params.lua
@@ -0,0 +1,8 @@
+-- Parameter configuration for Ventilator
+--
+-- Written by Michiel Fokke <michiel@fokke.org>
+--
+-- MIT license, http://opensource.org/licenses/MIT
+
+Param{ "humSP", "Humidity setpoint", "%" }
+Param{ "", "", "
